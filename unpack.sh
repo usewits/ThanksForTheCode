3a5b6f88104d89a786e5b0b7a013ff50d742374a
abe <abe@lightwits.localdomain>
2022-09-02 14:08:17 +0200

    Update unpack script.
    NOTE: it works on both windows and linux! Did not test mac but very
    likely to work since its unix.

--- unpack.sh
+++ unpack.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
+# This script should work on Linux, Windows and Mac
+
+echo "Will unzip ThanksForTheCode_archive to ThanksForTheCode"
+echo " - you'll need about 4GB free space"
+echo " - this will take a few minutes"
+sleep 1
+echo "."
+sleep 1
+echo ".."
+sleep 1
+echo "..."
+sleep 1
+echo "Unzipping..."
+
 cat ThanksForTheCode_archive/ThanksForTheCode.tar.bz2_* | tar -xj
+
+echo "Done!"
+echo "Feel free to exit this program."
+
+# Sleep so GUI users can see the above outputs:
+sleep 60
